2466b45637d84ee295726adcac4e4656b2927368
Tanishka Khandelwal <Khandelwalt072@gmail.com>
2026-05-01 22:59:03 +0530

    Add multiple programs for various tasks

--- homework day 1 main.cpp
+++ homework day 1 main.cpp
@@ -20,7 +20,7 @@ int main() {
     return 0;
 }
 
-// Program 2-- Alphabet case checker
+// Program 2 -- Alphabet Case Checker
 #include <iostream>
 using namespace std;
 
@@ -34,6 +34,60 @@ int main() {
         cout << "Uppercase";
     else
         cout << "Lowercase";
+
+    return 0;
+}
+
+Program 3 -- Age Group Classifier
+#include <iostream>
+using namespace std;
+
+int main() {
+    int age;
+
+    cout << "Enter age: ";
+    cin >> age;
+
+    if (age < 18)
+        cout << "Minor";
+    else
+        cout << "Adult";
+
+    return 0;
+}
+
+Program 4 -- Area of Shapes
+#include <iostream>
+using namespace std;
+
+int main() {
+    int length, breadth, area;
+
+    cout << "Enter length: ";
+    cin >> length;
+
+    cout << "Enter breadth: ";
+    cin >> breadth;
+
+    area = length * breadth;
+
+    cout << "Area = " << area;
+
+    return 0;
+}
+
+Program 5 -- Sum and Average of Three Numbers
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, b, c;
+
+    cout << "Enter 3 numbers: ";
+    cin >> a >> b >> c;
+
+    cout << "Sum = " << a + b + c << endl;
+    cout << "Average = " << (a + b + c) / 3;
 
     return 0;
 }
